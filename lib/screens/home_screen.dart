2534ea7883361428944fb9c3d8f4a01391e2a2ff
김광회 <postelian@gmail.com>
2023-01-09 11:28:26 +0900

    7.4.3 웹뷰 컨트롤러 설정하기

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  // StatelessWidget 생성자는 재사용 가능한 인스턴스 위젯으로 처리될 수 있도록 빌드타임 상수(const)를 붙여 메모리를 절약한다.
-  // 참고 => final: 런타임 상수, const: 빌드타임 상수
-  const HomeScreen({Key? key}) : super(key: key);
+  WebViewController? controller;
+
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,13 @@ class HomeScreen extends StatelessWidget {
         title: Text('Code Factory'),
         centerTitle: true,
       ),
-      body: Text('Home Screen'),
+      body: WebView(
+        onWebViewCreated: (WebViewController controller) {
+          this.controller = controller;
+        },
+        initialUrl: 'https://blog.codefactory.ai',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
   }
 }
